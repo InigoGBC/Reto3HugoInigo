002895ae41958890a8a1bc6e1451aececd5d7e6f
1DAM <1DAM@A11-PC15>
2025-11-19 13:39:58 +0100

    menu y funciones

--- Reto3HugoInigo/src/Reto/Funciones.java
+++ Reto3HugoInigo/src/Reto/Funciones.java
@@ -39,5 +39,7 @@ public class Funciones {
 		}
 		}while(true);
 	}
-}
+	
+	}
+
 
--- Reto3HugoInigo/src/Reto/Funciones1.java
+++ Reto3HugoInigo/src/Reto/Funciones1.java
@@ -4,11 +4,32 @@ import java.util.Scanner;
 
 public class Funciones1 {
 
+	public static String capitalizarString(String palabra) {
+		return palabra.substring(0,1).toUpperCase()+palabra.substring(1);
+		}
+	
+	public static String generarApodo(String palabra) {
+		if (palabra.length()<=4) {
+			return palabra+palabra.length()
+;		}
+			
+		return palabra.substring(0, 3)+palabra.charAt(palabra.length()-1)+palabra.length();
+	
+	}
+	
 	
 	public static void menu1(Scanner sc) {
+		String nombre;
+		
 		do {
 			System.out.println("dime un nombre ");
-			
-		} while (condition);
+		 nombre = sc.nextLine();
+		} while (!("".isEmpty()));
+		
+		System.out.println(Funciones1.capitalizarString(nombre));
+		System.out.println("apodo: " + Funciones1.generarApodo(nombre));
+		
 	}
+	
 }
+
