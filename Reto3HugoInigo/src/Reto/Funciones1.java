977088f18439aa82ea8b4b36e31846d549dc178f
1DAM <1DAM@A11-PC15>
2025-11-19 12:31:55 +0100

    Merge branch 'main' of https://github.com/InigoGBC/Reto3HugoInigo.git into main

--- Reto3HugoInigo/src/Reto/Funciones1.java
+++ Reto3HugoInigo/src/Reto/Funciones1.java
@@ -1,5 +1,14 @@
 package Reto;
 
+import java.util.Scanner;
+
 public class Funciones1 {
 
+	
+	public static void menu1(Scanner sc) {
+		do {
+			System.out.println("dime un nombre ");
+			
+		} while (condition);
+	}
 }
